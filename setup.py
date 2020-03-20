771097259c16ff07e25aeacd9e56b317b1fb0ded
Imran Ashraf <iimran.aashraf@gmail.com>
2020-03-20 13:55:00 +0100

    added travis CI config

--- setup.py
+++ setup.py
@@ -65,10 +65,10 @@ def build_libqasm_library(make_command: str, cmake_options: str) -> None:
         cmake_options: additional build options to pass to cmake.
     """
     os.chdir(build_dir)
-    execute_process(f'git submodule update --init --recursive')
-    execute_process(f'cmake {cmake_options} {os.path.join("..", "library")}')
-    execute_process(f'{make_command}')
-    execute_process(f'{make_command} test')
+    execute_process('git submodule update --init --recursive')
+    execute_process('cmake {cmake_options} {os.path.join("..", "library")}')
+    execute_process('{make_command}')
+    execute_process('{make_command} test')
     os.chdir(root_dir)
 
 
